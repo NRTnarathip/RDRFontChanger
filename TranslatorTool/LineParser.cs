ea5c05348160478e6d4716ba35f3211c46653df0
NRTnarathip <61742272+NRTnarathip@users.noreply.github.com>
2025-12-20 05:12:41 +0700

    WIP: LineParser

---
+++ TranslatorTool/LineParser.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Permissions;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace TranslatorTool;
+
+public sealed class LineParser
+{
+    public enum TextType : int
+    {
+        HTMLTag,
+        Placeholder,
+        Text,
+    }
+    public readonly string m_prefix;
+    public readonly string m_text;
+    public readonly int m_index = -1;
+    public readonly List<string> textSplits = new();
+    public readonly List<TextType> textTypeSplits = new();
+    public readonly bool isPureText;
+    public readonly bool isPureSingleSymbol;
+
+    public LineParser(string line)
+    {
+        line = line.Trim();
+        // assert check!
+        int openBracket = line.IndexOf('[');
+        int closeBracket = line.IndexOf(']');
+        int dashIndex = line.IndexOf('-');
+        if (dashIndex == -1)
+            return;
+
+        bool indexParseCorrect = int.TryParse(line.Substring(
+            openBracket + 1,
+            closeBracket - openBracket - 1),
+            out int index) && index >= 0;
+        if (!indexParseCorrect)
+        {
+            return;
+        }
+
+        // ready!
+        this.m_prefix = line.Substring(0, dashIndex + 1);
+        this.m_text = line.Substring(dashIndex + 1).Trim();
+        this.m_index = index;
+
+        Console.WriteLine("parse line: " + line);
+
+        // try parse tag symbol
+        if (IsAnyTagSymbol(m_text))
+        {
+            var currentText = m_text;
+            while (currentText.Length > 0)
+            {
+                int moveNext = 1;
+                // like <A> Hello World  </A>
+                var c = currentText[0];
+                // find html tag first!
+                if (c == '<')
+                {
+                    if (TryParseHTMLTag(currentText,
+                        out var content, out var beginTag, out var endTag))
+                    {
+                        Console.WriteLine($"found tag: {beginTag}{content}{endTag}");
+                        moveNext = content.Length + beginTag.Length + endTag.Length;
+                        MarkText(content, TextType.HTMLTag);
+                    }
+                }
+                // like {@RADIAL_MENU.SHOW_HIDE:PENDING}
+                else if (c == '{')
+                {
+                    if (TryParsePlaceholder(currentText,
+                        out var content, out var beginIndex))
+                    {
+                        Console.WriteLine($"found ui prompt tag: {content}");
+                        moveNext = beginIndex + content.Length;
+                        MarkText(content, TextType.Placeholder);
+                    }
+                }
+                // normal text content
+                else
+                {
+                    var firstAnyTagIndex = Regex.Match(currentText, "[<{]");
+                    if (firstAnyTagIndex.Success)
+                    {
+                        var content = currentText.Substring(0, firstAnyTagIndex.Index);
+                        moveNext = content.Length;
+                        MarkText(content, TextType.Text);
+                        Console.WriteLine($"found content text: {content}");
+                    }
+                    else
+                    {
+                        // all current text is pure text
+                        moveNext = currentText.Length;
+                        MarkText(currentText, TextType.Text);
+                        Console.WriteLine($"found pure text: {currentText}");
+                    }
+                }
+
+                // Console.WriteLine("move next: " + moveNext);
+                if (moveNext >= currentText.Length)
+                    break;
+                currentText = currentText.Substring(moveNext);
+            }
+        }
+
+        // helper
+        isPureText = true;
+        foreach (var str in textTypeSplits)
+        {
+            if (str != TextType.Text)
+                isPureText = false;
+        }
+
+        isPureSingleSymbol = textTypeSplits.Count == 1
+                && textTypeSplits[0] != TextType.Text;
+    }
+
+    void MarkText(string text, TextType type)
+    {
+        this.textSplits.Add(text);
+        this.textTypeSplits.Add(type);
+    }
+
+    public static bool TryParsePlaceholder(
+        string text,
+        out string content,
+        out int beginIndex)
+    {
+        content = "";
+        beginIndex = -1;
+
+        //  example
+        //  {@RADIAL_MENU.SHOW_HIDE:PENDING}
+        //  {0}
+        var tag = TryFindFirstTag(text,
+            ['{', '}'], out beginIndex, out var endIndex);
+        if (tag.Length > 0)
+        {
+            content = text.Substring(beginIndex, endIndex - beginIndex + 1);
+        }
+
+        return content.Length > 0;
+    }
+
+    public static bool TryParseHTMLTag(string inputText,
+        out string content,
+        out string beginTag, out string endTag)
+    {
+        beginTag = "";
+        endTag = "";
+        content = "";
+
+        // at lest 3 char like: <A>
+        if (inputText.Length < 3)
+            return false;
+
+        beginTag = TryFindFirstHTMLTag(inputText, out var beginIndexFirst, out var beginIndexLast);
+        if (beginTag.Length == 0)
+            return false;
+
+        var nextText = inputText.Substring(beginIndexLast + 1);
+        endTag = TryFindFirstHTMLTag(nextText, out var endIndexFirst, out var endIndexLast);
+        if (endTag.Length == 0)
+            return false;
+
+        content = nextText.Substring(0, endIndexFirst);
+        return true;
+    }
+
+    public static string TryFindFirstHTMLTag(string text,
+            out int beginIndex, out int endIndex)
+    {
+        return TryFindFirstTag(text, ['<', '>'], out beginIndex, out endIndex);
+    }
+
+    public static string TryFindFirstTag(string text,
+           char[] tagCarBeginEnd,
+           out int beginIndex, out int endIndex)
+    {
+        beginIndex = endIndex = -1;
+
+        if (tagCarBeginEnd.Length != 2)
+            return "";
+
+        beginIndex = text.IndexOf(tagCarBeginEnd[0]);
+        endIndex = text.IndexOf(tagCarBeginEnd[1]);
+        if (beginIndex == -1 || endIndex == -1
+            || endIndex < beginIndex)
+            return "";
+        return text.Substring(beginIndex, endIndex - beginIndex + 1);
+    }
+
+    public static bool IsAnyTagSymbol(string t)
+    {
+        // <a>WW.XX</a>
+        // <0xfcaf17>CHALLENGE:</0x>
+        // {0} {@UI.xxx} 
+        const string pattern = "[<{]";
+        return Regex.IsMatch(t, pattern);
+    }
+}
--- TranslatorTool/Program.cs
+++ TranslatorTool/Program.cs
@@ -3,168 +3,20 @@ using OpenAI.Chat;
 using System.ClientModel;
 using System.Diagnostics;
 using System.Text.RegularExpressions;
+using TranslatorTool;
 
-var options = new OpenAIClientOptions
+internal class Program
 {
-    Endpoint = new Uri("http://localhost:1234/v1")
-};
-
-ChatClient client = new ChatClient("typhoon-translate-4b",
-    new ApiKeyCredential("aweasd"), options);
-
-var dir = "D:\\SteamLibrary\\steamapps\\common\\Red Dead Redemption\\mods\\localize";
-var filename = "interface_win32.txt";
-var srcFilePath = Path.Combine(dir, filename);
-var saveFilePath = Path.Combine(dir, filename.Replace(".txt", "@thai-ai.txt"));
-if (File.Exists(saveFilePath) == false)
-    File.Copy(srcFilePath, saveFilePath);
-
-Stopwatch saveFileTimer = new Stopwatch();
-saveFileTimer.Start();
-int saveFileEverySeconds = 5;
-var writeLines = File.ReadAllLines(saveFilePath);
-int totalLine = writeLines.Length;
-for (int lineIndex = 0; lineIndex < totalLine; lineIndex++)
-{
-    var line = writeLines[lineIndex];
-    if (line.Length == 0)
-        continue;
-
-    // already translate it
-    bool hasThai = Regex.IsMatch(line, @"[\u0E00-\u0E7F]");
-    if (hasThai)
-        continue;
-
-    int openBracket = line.IndexOf('[');
-    int closeBracket = line.IndexOf(']');
-    int dashIndex = line.IndexOf('-');
-    if (dashIndex == -1)
-        continue;
-
-    string prefix = line.Substring(0, dashIndex + 1);
-    string text = line.Substring(dashIndex + 1).Trim();
-
-    // $/content/scripting/DesignerDefined/SocialClub/sc_challenge_02
-    if (text.StartsWith("$/"))
+    private static async Task Main(string[] args)
     {
-        Console.WriteLine("skip: " + line);
-        continue;
-    }
-
-    string textPartPattern = @"(<[^>]+>|\{[^}]+\})";
-    string[] textSplits = Regex.Split(
-        text, textPartPattern)
-          .Where(s => !string.IsNullOrEmpty(s) && !Regex.IsMatch(s, @"^\w+$")).ToArray();
-
-    // check if found any string
-    // if not found we should stop it!
-    bool foundAnyWord = false;
-    foreach (string p in textSplits)
-    {
-        var part = p.Trim();
-        // like "  "
-        if (part.Length == 0)
-            continue;
-
-        // such as:  <A>  {@UI.xxx}
-        if (part.StartsWith("<") || part.StartsWith("{"))
-            continue;
-
-        foundAnyWord = true;
-        break;
-    }
-
-    if (!foundAnyWord)
-    {
-        Console.WriteLine($"skip: {line}");
-        continue;
-    }
-
-    // try translate this line
-    string translateText = "";
-    while (true)
-    {
-        translateText = await TranslateTextAsync(client, text);
-        string[] translateTextSplits = translateText.Split(
-            ["\r\n", "\r", "\n"], StringSplitOptions.None);
-        Console.WriteLine("retry...");
-        // only 1 line!
-        if (translateTextSplits.Length != 1
-            || translateText.Contains("ภาษาไทย"))
+        var filename = "interface_win32.txt";
+        var lines = File.ReadAllLines(filename);
+        foreach (var line in lines)
         {
-            Console.WriteLine("look like your AI result is incorrect");
-            Console.WriteLine($"result: {translateText}");
-            translateText = ""; //clear
-            continue;
+            var lineParse = new LineParser(line);
+            if (lineParse.isPureSingleSymbol)
+                Console.WriteLine($"skip this line: {line}, don't translate");
         }
-
-        break;
-    }
-
-    if (translateText.Length == 0)
-    {
-        await EjectModelAsync();
-        Console.WriteLine("stop translate!");
-        break;
-    }
-
-    string indexStr = line.Substring(openBracket + 1, closeBracket - openBracket - 1);
-    int index = int.Parse(indexStr);
-    var newLine = $"[{index}] - {translateText}";
-    writeLines[lineIndex] = newLine;
-    Console.WriteLine($"write line: " + newLine);
-
-    if (saveFileTimer.Elapsed.TotalSeconds >= saveFileEverySeconds)
-    {
-        Console.WriteLine($"progress line {lineIndex + 1} / {totalLine} total");
-        Console.WriteLine("try saving file...");
-        File.WriteAllLines(saveFilePath, writeLines);
-        Console.WriteLine("saved file!");
-        saveFileTimer.Restart(); //reset it!
-    }
-}
-
-File.WriteAllLines(saveFilePath, writeLines);
-Console.WriteLine("success translate all lines!");
-Console.Read();
-
-async Task EjectModelAsync()
-{
-    Console.WriteLine("Try Eject model...");
-    using (var client = new HttpClient())
-    {
-        var requestUri = "http://localhost:1234/v1/models/eject";
-        try
-        {
-            var response = await client.PostAsync(requestUri, null);
-            if (response.IsSuccessStatusCode)
-            {
-                Console.WriteLine("Model ejected successfully.");
-            }
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Error: {ex.Message}");
-        }
-    }
-}
-
-async Task<string> TranslateTextAsync(ChatClient client, string text)
-{
-    List<ChatMessage> messages = [
-        new SystemChatMessage($"Translate to Thai. " +
-            $"Provide ONLY the translated text without explanations or quotes. " +
-            $"Output only 1 line. "),
-        new UserChatMessage(text)
-    ];
-
-    try
-    {
-        ChatCompletion completion = await client.CompleteChatAsync(messages);
-        return completion.Content[0].Text.Trim();
-    }
-    catch (Exception ex)
-    {
-        return $"[Error: {ex.Message}]";
+        Console.Read();
     }
 }
